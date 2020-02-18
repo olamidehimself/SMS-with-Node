91187685a4aeda2222dae897fa17bb0e26e23fbc
olamidehimself <egundeyiolamide@gmail.com>
2020-02-18 17:11:32 +0100

    conncted to mongoDB and created routes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-// const connectDB = require('../');
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+connectDB();
 
-app.get('/', (req, res) => res.send('Working'))
+app.get('/', (req, res) => res.send('Working'));
+app.use('/api/users', require('./routes/api/users'));
 app.listen(PORT, () => console.log(`Server started on port ${5000}`));
